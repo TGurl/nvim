0ec0fd5f1e0dafc3bad71f49a6a5b67b67f5b565
Geertje van Dijk <geertjevandijk70@gmail.com>
2021-09-26 15:57:05 +0200

    enabled CTRL-S

--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -1,3 +1,5 @@
+-- Keymap settings --
+--
 -- set space as leader key
 vim.api.nvim_set_keymap('n', '<Space>', '<NOP>', { noremap = true , silent = true })
 vim.g.mapleader = ' '
@@ -7,3 +9,9 @@ vim.api.nvim_set_keymap('n', '<Leader>h', ':set hlsearch!<CR>', { noremap = true
 
 -- toggle nvim-tree
 vim.api.nvim_set_keymap('n', '<Leader>e', ':NvimTreeToggle<CR>', { noremap = true , silent = true })
+
+-- enable CTRL+S
+vim.api.nvim_set_keymap('n', '<C-s>', ':w<CR>', { noremap = true, silent = false })
+vim.api.nvim_set_keymap('i', '<C-s>', ':w<CR>', { noremap = true, silent = false })
+vim.api.nvim_set_keymap('v', '<C-s>', ':w<CR>', { noremap = true, silent = false })
+
